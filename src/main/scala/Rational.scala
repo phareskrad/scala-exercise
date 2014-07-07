a4ea03aa888cc28a79c192230cce37ce489775ae
Leon Liao <Leon@Leons-MacBook-Pro.local>
2014-07-06 21:23:39 -0400

    Chapter 6 completed

---
+++ src/main/scala/Rational.scala
@@ -0,0 +1,50 @@
+/**
+ * Created by Leon on 6/30/14.
+ */
+class Rational(n: Int, d: Int)
+{
+  require(d != 0)
+
+  private val g = gcd(n.abs, d.abs)
+  val numer: Int = n / g
+  val denom: Int = d / g
+
+  def this(n: Int) = this(n, 1)
+
+  override def toString = numer +"/"+ denom
+
+  def + (that: Rational): Rational =
+    new Rational(
+      numer * that.denom + that.numer * denom,
+      denom * that.denom
+    )
+
+  def + (i: Int): Rational =
+    new Rational(numer + i * denom, denom)
+
+  def - (that: Rational): Rational =
+    new Rational(
+      numer * that.denom - that.numer * denom,
+      denom * that.denom
+    )
+
+  def - (i: Int): Rational =
+    new Rational(numer - i * denom, denom)
+
+  def * (that: Rational): Rational =
+    new Rational(numer * that.numer, denom * that.denom)
+
+  def * (i: Int): Rational =
+    new Rational(numer * i, denom)
+
+  def / (that: Rational): Rational =
+    new Rational(numer * that.denom, denom * that.numer)
+
+  def / (i: Int): Rational =
+    new Rational(numer, denom * i)
+
+  private def gcd(a: Int, b: Int): Int =
+    if(b == 0) a else gcd(b, a % b)
+
+
+}
